a8171a19c250904248554c81a749abdee50ca055
Sathwik <sathwiknamuu@gmail.com>
2025-09-01 17:57:46 +0530

    task 1

---
+++ Week 1 Task.py
@@ -0,0 +1,10 @@
+Principal_amount = 5000
+Rate_of_interest = 8.5 / 12
+Time_in_months = 18
+Simple_interest_earned = (Principal_amount*Rate_of_interest*Time_in_months)/100
+Total_amount = Principal_amount + Simple_interest_earned
+print("Principal_amount: $",Principal_amount)
+print("Rate_of_interest: ",Rate_of_interest,"%")
+print("Time_in_months: ",Time_in_months)
+print("Simple_interest_earned: $",Simple_interest_earned)
+print("Total_amount: $",Total_amount)
